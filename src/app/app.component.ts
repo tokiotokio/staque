7eca61c2ef6c37a331406139b3748f9a94bb4a5f
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-04-23 10:12:29 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'game';
+
+	scene:Boolean = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { GameComponent } from './game/game.component';
+import { StackComponent } from './game/stack/stack.component';
+import { QueueComponent } from './game/queue/queue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    GameComponent,
+    StackComponent,
+    QueueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
