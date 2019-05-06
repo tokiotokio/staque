21a41b4fd8fc6cfff2bd3f13895aaf99c75e9958
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-05-07 06:23:19 +0900

    add fig to title

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var require: any;
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
+
+	fig_title = require("../image/title.png");
+	fig_game = require("../image/newGame.png");
 
   constructor() { }
 
