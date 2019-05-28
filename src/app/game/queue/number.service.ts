9f46a53abae4d67c5305450787c3f5d04c44c691
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-05-28 09:25:10 +0900

    #1 add queue value

--- src/app/game/queue/number.service.ts
+++ src/app/game/queue/number.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable,of,Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NumberService {
 
+	queue1_:number[] = [1,2,3,4];
+	queue2_:number[] = [2,2,3,4];
+	queue3_:number[] = [3,2,3,4];
+	queue4_:number[] = [4,2,3,4];
+	queue1:Subject<number[]> = new Subject<number[]>();
+	queue2:Subject<number[]> = new Subject<number[]>();
+	queue3:Subject<number[]> = new Subject<number[]>();
+	queue4:Subject<number[]> = new Subject<number[]>();
+
   constructor() { }
+	init() :void{
+		this.queue1.next(this.queue1_);
+		this.queue2.next(this.queue2_);
+		this.queue3.next(this.queue3_);
+		this.queue4.next(this.queue4_);
+	}
 }
--- src/app/game/queue/queue.component.ts
+++ src/app/game/queue/queue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NumberService } from './number.service';
 
 declare var require: any;
 
@@ -10,10 +11,25 @@ declare var require: any;
 export class QueueComponent implements OnInit {
 
 	fig_queue = require('../../image/queue1.png');
+	queue1:number[];
+	queue2:number[];
+	queue3:number[];
+	queue4:number[];
 
-  constructor() { }
+	constructor(
+		private number_s : NumberService
+	){}
 
   ngOnInit() {
+		this.number_s.queue1
+			.subscribe(stk => this.queue1= stk);
+		this.number_s.queue2
+			.subscribe(stk => this.queue2= stk);
+		this.number_s.queue3
+			.subscribe(stk => this.queue3= stk);
+		this.number_s.queue4
+			.subscribe(stk => this.queue4= stk);
+		this.number_s.init();
   }
 
 	runQueue():void {
